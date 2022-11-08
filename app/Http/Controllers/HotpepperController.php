fc9e6225146edd5af012e597d423cfbb9caff457
marumarumorimori <kiki20150330@gmail.com>
2022-11-08 09:57:03 +0900

    HotpepperController.phpの作成

---
+++ app/Http/Controllers/HotpepperController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client; // ここを追記
+
+class HotpepperController extends Controller
+{
+
+    // HTTPリクエストを送るURL
+    private const REQUEST_URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/';
+
+    // APIキー
+    private $api_key;
+
+    public function index()
+    {
+        // インスタンス生成
+        $client = new Client();
+
+        // HTTPリクエストメソッド
+        $method = 'GET';
+
+        // APIキーを取得
+        $this->api_key = config('hotpepper.api_key');
+
+        // APIキーや検索ワードなどの設定を記述
+        $options = [
+            'query' => [
+                'key' => config('hotpepper.api_key'),
+                'keyword' => '新宿',
+                'count' => 10,
+                'format' => 'json',
+            ],
+        ];
+
+        // HTTPリクエストを送信
+        $response = $client->request($method, self::REQUEST_URL, $options);
+
+        // 'format' => 'json'としたのでJSON形式でデータが返ってくるので、連想配列型のオブジェクトに変換
+        $restaurants = json_decode($response->getBody(), true)['results'];
+
+        // index.blade.phpを表示する
+        return view('index', compact('restaurants'));
+    }
+
+}
