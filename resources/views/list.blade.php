05fe4ceb8236a57fb4659e3e7185f243acf8e7ee
marumarumorimori <kiki20150330@gmail.com>
2022-11-14 23:41:25 +0900

    デザイン修正

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -4,6 +4,7 @@
 @extends('layouts.parent')
 @section('title', '一覧')
 @section('content')
+
     <div class="shop-container">
         <div class="shop-container-text">
             <p>検索結果 <span>{{ $restaurants['results_available'] }}</span>件</p>
@@ -26,13 +27,38 @@
             </div>
         @endfor
     </div>
-    <?php $page_num = ceil($restaurants['results_available']/$count);?>
-    <div class="pagination">
-        <ul class="pagination__list">
-            @for ($i = 0; $i < $page_num; $i++)
-                <?php $current_page = $count * $i + 1; ?>
-                <li class="pagination__item"><a href=""><a href="{{route('search', [ 'range' => $range ,'lat' => $lat,'lng' => $lng,'start' => $current_page])}}"><?php echo $i + 1; ?></a></a></li>
-            @endfor
-        </ul>
+
+    <?php
+        $page_num = ceil($restaurants['results_available']/$count);
+        $current_page = ceil($restaurants['results_start']/$count);
+        $display_page = 5;
+        $begin_page = 1;
+        $finish_page = $display_page;
+        $center_page=floor($display_page / 2);
+        $prev = max(($current_page - 2) * $count + 1 , 1);
+        $next = min(($current_page) * $count + 1 , ($page_num - 1) * $count + 1);
+    ?>
+    @if($current_page > $center_page)
+        <?php
+            $begin_page = $current_page - $center_page;
+            $finish_page = $current_page +$center_page;
+        ?>
+    @endif
+    <?php $finish_page = min($page_num , $finish_page); ?>
+    <div class="pagination__wrapper">
+        <nav class="pagination">
+            <ul class="page-numbers">
+                <li><a href="{{route('search', [ 'range' => $range ,'lat' => $lat,'lng' => $lng,'start' => $prev])}}" class="pagination-next page-numbers"><i class="fa fa-chevron-left"></i></a></li>
+                    @for ($i = $begin_page; $i <= $finish_page; $i++)
+                        <?php $page_start = $count * ($i - 1) + 1; ?>
+                        @if($i == $current_page )
+                            <li><span aria-current="page" class="page-numbers pagenation-current"><?php echo $i; ?></span></li>
+                        @else
+                            <li class="page-numbers"><a href="{{route('search', [ 'range' => $range ,'lat' => $lat,'lng' => $lng,'start' => $page_start])}}"><?php echo $i; ?></a></li>
+                        @endif
+                    @endfor
+                <li><a href="{{route('search', [ 'range' => $range ,'lat' => $lat,'lng' => $lng,'start' => $next])}}" class="pagination-next page-numbers"><i class="fa fa-chevron-right"></i></a></li>
+            </ul>
+        </nav>
     </div>
 @endsection
